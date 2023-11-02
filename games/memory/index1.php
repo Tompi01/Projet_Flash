dd3868cf9215a039d68f275a651a5f9ea0345121
Temayoo <matt.yo95280@gmail.com>
2023-11-02 09:23:02 +0100

    Story 1-2 80% fini

--- games/memory/index1.php
+++ games/memory/index1.php
@@ -1,78 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
 <?php 
 require_once '../../utils/common.php';
 require_once SITE_ROOT. 'partials/head.php';
 require_once SITE_ROOT. 'partials/header.php';
-
 ?>
 
-
 <section class="theme">
-<body>
-<section class="theme">
+  <body>
+    <section class="theme">
+
+      <!-------------------------------------------BANNER---------------------------------------------------->
+
+      <div class="banner">
+        <h1>JEU</h1>
+      </div>
+
+      <!-------------------------------------------FIN BANNER---------------------------------------------------->
+
+      <div id="themeCarte">
+        <div id="themeClassique">
+          <span>Sélectionnez le niveau :</span>
+          <select id="levelSelect">
+            <option value="4">4x4</option>
+            <option value="8">8x8</option>
+            <option value="12">12x12</option>
+          </select>
+          <span>Sélectionnez le thème :</span>
+          <select id="themeSelect">
+            <option value="Default">Thème League of Legends</option>
+            <option value="Pokemon">Thème Pokemon</option>
+            <option value="ClashRoyale">Thème ClashRoyale</option>
+          </select>
+          <button id="generateGrid">Générer la grille/Lancer la Partie</button>
+        </div>
+      </div>
+
+      <div class="grid-container">
+
+      <table id="memoryGrid"></table> 
+
+      </div>
 
 
 
 
+    </section>
 
-
-
-<!-------------------------------------------BANNER---------------------------------------------------->
-
-<div class="banner">
-    <h1>CHOIX LEVEL</h1>
-</div>
-
-<!-------------------------------------------FIN BANNER---------------------------------------------------->
-
-
-<center> <br> <h1> CARTE BASIQUE</h1></center>
-<!-------------------------------------------DIFFERENT LEVEL THEME 1---------------------------------------------------->
-<div class="different-level">
-    <div class="level1"><p><a href="memory1T1.php"><strong>LEVEL I</strong></a></p></div>
-    <div class="level2"><p><a href="memory2T1.php"><strong>LEVEL II</strong></a></p></div>
-    <div class="level3"><p><a href="memory3T1.php"><strong>LEVEL III</strong></a></p></div>
-</div>
-<!-------------------------------------------FIN DIFFERENT LEVEL THEME 1 ---------------------------------------------------->
-
-<center> <br> <h1>POKEMON</h1></center>
-
-<!-------------------------------------------DIFFERENT LEVEL THEME 2---------------------------------------------------->
-<div class="different-level">
-    <div class="level1"><p><a href="memory1T2.php"><strong>LEVEL I</strong></a></p></div>
-    <div class="level2"><p><a href="memory2T2.php"><strong>LEVEL II</strong></a></p></div>
-    <div class="level3"><p><a href="memory3T2.php"><strong>LEVEL III</strong></a></p></div>
-</div>
-<!-------------------------------------------FIN DIFFERENT LEVEL THEME 2 ---------------------------------------------------->
-
-<center> <br> <h1> YU GI OH </h1></center>
-
-<!-------------------------------------------DIFFERENT LEVEL THEME 3---------------------------------------------------->
-<div class="different-level">
-    <div class="level1"><p><a href="memory1T3.php"><strong>LEVEL I</strong></a></p></div>
-    <div class="level2"><p><a href="memory2T3.php"><strong>LEVEL II</strong></a></p></div>
-    <div class="level3"><p><a href="memory3T3.php"><strong>LEVEL III</strong></a></p></div>
-</div>
-
-
-<!-------------------------------------------FIN DIFFERENT LEVEL THEME 3 ---------------------------------------------------->
-
-
-
-
-
-
- 
- </body>
-    <!-------------------------------------------FOOTER---------------------------------------------------->
-<?php 
-require_once SITE_ROOT. 'partials/footer.php';
-?>
-
-    <!-------------------------------------------FIN FOOTER---------------------------------------------------->
-
-</section>
+    <script src="../../styles/script.js"></script>
+  </body>
 </html>
